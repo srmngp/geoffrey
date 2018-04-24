1b92a3b163ca579400ec9930ac9979a70a8f3190
MGP <sr.mngp@gmail.com>
2018-04-24 12:36:18 +0200

    añdado geoffreyExcepction y un login bonico

--- src/main/java/es/alfatecsistemas/glpinfo/geoffrey/bean/TareaBean.java
+++ src/main/java/es/alfatecsistemas/glpinfo/geoffrey/bean/TareaBean.java
@@ -1,5 +1,13 @@
 package es.alfatecsistemas.glpinfo.geoffrey.bean;
 
+import javax.faces.bean.RequestScoped;
+
+import org.springframework.stereotype.Component;
+
+@Component
+@RequestScoped
 public class TareaBean {
+	
+	
 
 }
---
+++ src/main/java/es/alfatecsistemas/glpinfo/geoffrey/support/GeoffreyException.java
@@ -0,0 +1,10 @@
+package es.alfatecsistemas.glpinfo.geoffrey.support;
+
+public class GeoffreyException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public GeoffreyException(String message) {
+		super(message);
+	}
+}
