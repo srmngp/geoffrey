087ef3fc503c99ccb7d531d7a6c9125b318c3779
MGP <sr.mngp@gmail.com>
2018-04-24 12:46:39 +0200

    actualizacion de imports

--- src/main/java/es/alfatecsistemas/glpinfo/geoffrey/model/entity/Usuario.java
+++ src/main/java/es/alfatecsistemas/glpinfo/geoffrey/model/entity/Usuario.java
@@ -3,7 +3,6 @@ package es.alfatecsistemas.glpinfo.geoffrey.model.entity;
 import java.util.List;
 
 import javax.persistence.CascadeType;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
